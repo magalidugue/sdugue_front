2a3f08ea064e2f7543c8cd825c581fea919ebecb
Magali Dugué <77433765+magalidugue@users.noreply.github.com>
2022-03-14 08:14:00 +0100

    routing set up

--- src/components/VisitCard.jsx
+++ src/components/VisitCard.jsx
@@ -1,34 +1,40 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "../css/visitCard.css";
 import classicalVisitImg from "../assets/church-window-gff159cb2f_1920.jpg";
 import adultWorkshopImg from "../assets/glass-g14fe2552c_1920.jpg";
 import kidWorkshopImg from "../assets/stained-glass-g2cf2f32b6_1920.jpg";
+import Welcome from "./welcome";
+import NavBar from "./Navbar";
 
 const VisitCard = () => {
   return (
-    <div className="container">
-      <div className="image">
-        <img className="image-img" src={classicalVisitImg} alt="" />
-        <div className="image-overlay">
-          <div className="image-title"> Visites Classiques </div>
-          <p className="image-description">[description]</p>
+    <React.Fragment>
+      <NavBar />
+      <Welcome />
+      <div className="container">
+        <div className="image">
+          <img className="image-img" src={classicalVisitImg} alt="" />
+          <div className="image-overlay">
+            <div className="image-title"> Visites Classiques </div>
+            <p className="image-description">[description]</p>
+          </div>
+        </div>
+        <div className="image">
+          <img className="image-img" src={adultWorkshopImg} alt="" />
+          <div className="image-overlay">
+            <div className="image-title"> Ateliers Adultes </div>
+            <p className="image-description">[description]</p>
+          </div>
+        </div>
+        <div className="image">
+          <img className="image-img" src={kidWorkshopImg} alt="" />
+          <div className="image-overlay">
+            <div className="image-title"> Atelier Enfants </div>
+            <p className="image-description">[description]</p>
+          </div>
         </div>
       </div>
-      <div className="image">
-        <img className="image-img" src={adultWorkshopImg} alt="" />
-        <div className="image-overlay">
-          <div className="image-title"> Ateliers Adultes </div>
-          <p className="image-description">[description]</p>
-        </div>
-      </div>
-      <div className="image">
-        <img className="image-img" src={kidWorkshopImg} alt="" />
-        <div className="image-overlay">
-          <div className="image-title"> Atelier Enfants </div>
-          <p className="image-description">[description]</p>
-        </div>
-      </div>
-    </div>
+    </React.Fragment>
   );
 };
 
